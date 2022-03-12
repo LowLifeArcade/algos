2b968556a04ec185cc5df192c9b72f2e136d3527
Sonny Brown <lowlifearcade@gmail.com>
2022-03-12 11:12:48 -0800

    test

--- BST.js
+++ BST.js
@@ -1,5 +1,47 @@
 class Node {
   constructor(value) {
     this.value = value;
+    this.left = null;
+    this.right = null;
   }
 }
+
+class BST {
+  constructor() {
+    this.root = null;
+  }
+  insert(value) {
+    let newNode = new Node(value);
+
+    if (this.root != null) {
+      // current starts as the root. This is key to understanding this. We will update currentNode as we loop
+      let currentNode = this.root;
+      while (true) {
+        if (value < currentNode.value) {
+          // we check to see if there's a node at all, not even the value. Just if it's there. If not, we've found our spot and we will update currentNode.left to the new node.
+          if (currentNode.left === null) {
+            currentNode.left = newNode;
+            return;
+          } else {
+            currentNode = currentNode.left 
+          }
+        }
+      }
+    }
+
+    if (this.root === null) {
+      this.root = newNode;
+      return this;
+    }
+  }
+}
+
+let myTree = new BST();
+myTree.insert(5);
+myTree.insert(4);
+myTree.insert(3);
+myTree.insert(2);
+myTree.insert(1);
+myTree.root.left.left.left.left
+
+console.log(myTree.root.left.left.left.left)
