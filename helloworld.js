f253eeb151c62e2f7b16ff9ad5292346964af5e9
Sonny Brown <lowlifearcade@gmail.com>
2022-03-23 21:13:20 -0700

    update

--- helloworld.js
+++ helloworld.js
@@ -1,8 +1,14 @@
-function recurse(num) {
-  if (num === 1) return 1
-  console.log('I ran' + num)
-  return num * recurse(num -1) 
-}
+// function recurse(num) {
+//   if (num === 1) return 1
+//   console.log('I ran' + num)
+//   return num * recurse(num -1) 
+// }
 
-console.log(recurse(16))
-console.log('Hello World')
+// console.log(recurse(16))
+// console.log('Hello World')
+
+let myStr = 'Hello: Bearer Token:jflajflajflajflafj'
+
+let token = myStr.split(' ')[2].split(':')[1] //?
+
+token
