fef4bfbfa0114b837e9f4b6b2c304d61f7386701
LowLifeArcade <68655112+LowLifeArcade@users.noreply.github.com>
2022-03-26 11:16:53 -0700

    update

---
+++ isprime.js
@@ -0,0 +1,30 @@
+function getNextPrime(inputValue) {
+  let nextNum = inputValue + 1;
+  let primeFound = false;
+  if (nextNum % 2 == 0) {
+    nextNum = nextNum + 1;
+  }
+  while (!primeFound) {
+    if (isPrime(nextNum)) {
+      primeFound = true;
+    } else {
+      nextNum = nextNum + 2;
+    }
+  }
+
+  return nextNum;
+}
+
+// 15
+function isPrime(value) {
+  let prime = true;
+  for (let i = 2; i < value; i++) {
+    if (value % i == 0) prime = false;
+  }
+  return prime;
+}
+
+
+// even not prime
+// check only odds
+// don't go past input value
