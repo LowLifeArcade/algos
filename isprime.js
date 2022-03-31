31f5c830231eb8833db6f3fa1b0ea22c8d9efae9
LowLifeArcade <68655112+LowLifeArcade@users.noreply.github.com>
2022-03-30 17:18:56 -0700

    update

--- isprime.js
+++ isprime.js
@@ -28,3 +28,34 @@ function isPrime(value) {
 // even not prime
 // check only odds
 // don't go past input value
+
+
+// original question
+
+// a prime is a number divisible by itself and 1 5 is prime 6 is not 
+// In Javascript, the modulus function is '%' - e.g. 5%2 gives 1 and 6%2 gives 0.
+
+function getNextPrime(inputValue) { 
+  let nextNum = inputValue + 1;
+  let primeFound = false;
+  while (!primeFound) {
+    if (isPrime(nextNum)) {
+      primeFound = true;
+    } else {
+      nextNum = nextNum + 1;
+    }
+  }
+  return nextNum;
+}
+
+function isPrime(value) {
+  for (let i=2; i < value; i++) {
+    if (value % i == 0) {
+      return false;
+    } else {
+      return true;
+    }
+  }
+} 
+
+// 
